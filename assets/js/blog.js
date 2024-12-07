b8aaa2ff92748e38de90bbf67a38752c12101708
jaornelas <ornelalf001@gmail.com>
2024-12-06 23:51:05 -0600

    main layout display

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,21 +1,42 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
 const main = document.querySelector("main");
-const backButton = document.querySelector("back-button")
+const backButton = document.querySelector(".back-button")
 
 // TODO: Create a function that builds an element and appends it to the DOM
-function buildEl() {
+const buildElement = function (element, text, className) {
+    const el = document.createElement(element);
+    el.textContent = text;
+    el.className = className;
+    main.appendChild(el);
+    }
 
-};
 
 // TODO: Create a function that handles the case where there are no blog posts to display
-function caseHandler() {
-
-};
+const noPosts = function () {
+    buildElement('h3', 'No posts to dipslay', 'no-posts');
+}
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
-renderBlog();
+const renderBlog = () => {
+    const blogData = readLocalStorage();
+
+    if (!blogData.length) {
+        noPosts();
+        return;
+    }
+
+    blogData.forEach((post) => {
+        buildElement('h3', post.name, 'post-name');
+        buildElement('p', post.comment, 'post-comment');
+    });
+};
+
 
 // TODO: Call the `renderBlogList` function
 renderBlogList();
+  // TODO: Implement the function body
 
-// TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
-redirectPage();
+
+backButton.addEventListener('click', function() {
+    redirectPage();
+});
+redirectPage()
