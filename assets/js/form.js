1474556729d9489affeeaf105a2fd496a1c9618c
jaornelas <ornelalf001@gmail.com>
2024-12-08 11:29:12 -0600

    store local data and redirect URL

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,24 @@
-// TODO: Create a variable that selects the form element
-const form = document.querySelector("form");
-//console.log(form);
+//TODO: Create a variable that selects the form element
+const form = document.querySelector('form');
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 const handleFormSubmit = function(event) {
     event.preventDefault();
     
-    const name = document.querySelector('#name').value;
-    const comment = document.querySelector('#comment').value;
+    const name = document.querySelector('#username').value;
+    const title = document.querySelector('#title').value;
+    const content = document.querySelector('#content');
 
-    if (!name || !comment) {
+    if (!name || !title || !content) {
         alert("Please fill out all fields.");
         return;
     }
 
-    const formData = { name, comment };
+    const formData = { 
+        name: name,
+        title: title,
+        content: content,
+    };
     localStorage.setItem('formData', JSON.stringify(formData));
 
     redirectPage('../blog.html');
@@ -28,10 +32,10 @@ const handleFormSubmit = function(event) {
 
 form.addEventListener("submit", handleFormSubmit);
 
-let redirectURL = '';
+// let redirectURL = '';
 
-const redirectPage = function(url) {
-    redirectURL = url;
-    location.assign(url);
-}
+// const redirectPage = function(url) {
+//     redirectURL = url;
+//     location.assign(url);
+// }
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,35 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+const toggleButton = document.getElementById('toggle');
+//const container = document.querySelectorAll('.container');
+
+// check local storage for mode preferance
+const currentMode = localStorage.getItem('mode') || 'light';
+
+//apply saved mode on page
+document.body.classList.add(`${currentMode}-mode`);
+//circle.classList.add(`${currentMode}-mode`);
+
+// Listen for a click event on toggle switch
+toggleButton.addEventListener('click', function () {
+  const newMode = document.body.classList.contains('light-mode') ? 'dark' : 'light';
+
+  //remove old mode and apply new 
+  document.body.classList.remove('light', 'dark');
+  //circle.classList.remove('light', 'dark');
+
+  document.body.classList.add(`${newMode}-mode`);
+  //circle.classList.add(`${newMode}-mode`);
+  
+  localStorage.setItem('mode', newMode);
+});
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
 function readLocalStorage() {
+  const data =localStorage.getItem(key);
+
+  //if data exists
+  return data ? JSON.parse(data) : [];
 
 };
 
